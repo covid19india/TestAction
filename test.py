f490ce4f01722c7391a758eb83c815e564f3f029
Junaid Babu <junaidvayoli@gmail.com>
2021-03-11 16:29:52 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import requests
 proxy = {"https":"http://35.244.23.205:3128"}
-vaccineDashboardNation = requests.request("get", "https://api.cowin.gov.in/api/v1/reports/getPublicReports?state_id=&district_id=&date=2021-03-01", proxies=proxy).json
+vaccineDashboardNation = requests.request("get", "https://api.cowin.gov.in/api/v1/reports/getPublicReports?state_id=&district_id=&date=2021-03-01", proxies=proxy).json()
 print(vaccineDashboardNation)
